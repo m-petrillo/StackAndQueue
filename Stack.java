33bb4c989e442ddf12bfd5540c5567fec4913948
Mike\Mike <th3champish3r3@gmail.com>
2017-04-01 19:02:24 -0400

    Setup push, pop, peek in Stack

--- Stack.java
+++ Stack.java
@@ -11,4 +11,65 @@ package stackandqueue.StackAndQueue;
  */
 public class Stack {
     
+    int maxSize;
+    int top;
+    String arr[];
+    
+    public Stack(int n)
+    {
+        maxSize = n;
+        arr = new String[maxSize];
+        top = 0;
+    }
+    
+    public boolean empty()
+    {
+      if(top == 0)
+      {
+          return true;
+      }
+      else 
+          return false;
+    }
+    
+    
+    
+    
+
+    public void push(String str)
+    {
+        
+    if(top < maxSize)
+    {
+    arr[top] = str;
+    top++;
+    }
+    }
+    
+    public String pop(String str)
+            {
+            if (!this.empty()
+                    {
+                
+                String temp = this.peek();
+                    }
+            
+            
+              if (top<maxSize)
+              {
+                  arr[top-1] = str;
+              }
+              else {
+                  return null;
+              }
+            }
+            
+public String peek()
+{
+   if(top > 0)
+   {
+    return arr[top-1]; 
+   }
+   else 
+       return null;
 }
